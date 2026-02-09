082dc5d4828d26726a7dd511c142b9ac9408c500
xintao <phil77@126.com>
2026-02-09 18:17:54 +0800

    test pytorch fa

--- tests/adapters.py
+++ tests/adapters.py
@@ -4,6 +4,8 @@ from typing import Type
 
 import torch
 
+from cs336_systems.flash_attn.fa_pytorch import FlashAttentionPytorchFunc
+
 
 
 def get_flashattention_autograd_function_pytorch() -> Type:
@@ -16,7 +18,7 @@ def get_flashattention_autograd_function_pytorch() -> Type:
         A class object (not an instance of the class)
     """
     # For example: return MyFlashAttnAutogradFunctionClass
-    raise NotImplementedError
+    return FlashAttentionPytorchFunc
 
 
 def get_flashattention_autograd_function_triton() -> Type:
