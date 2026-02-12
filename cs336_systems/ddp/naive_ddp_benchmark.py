f86bb336ce0dfa291fcc2078bb0f93a418e524c9
xintao <phil77@126.com>
2026-02-12 13:41:05 +0800

    set seed

--- cs336_systems/ddp/naive_ddp_benchmark.py
+++ cs336_systems/ddp/naive_ddp_benchmark.py
@@ -262,9 +262,11 @@ def main():
     seed = 123
 
     print("Single GPU + flash:")
+    torch.manual_seed(seed)
     model_use_flash = run_single(backend, cfg, use_flash=True, warmup=warmup, nsteps=nsteps, seed=seed)
 
     print("Single GPU + no flash:")
+    torch.manual_seed(seed)
     model_no_flash = run_single(backend, cfg, use_flash=False, warmup=warmup, nsteps=nsteps, seed=seed)
 
     print("Comparing: single_flash vs single_no_flash")
@@ -273,7 +275,8 @@ def main():
     print("Multi GPU + flash:")
     del model_no_flash
     torch.cuda.empty_cache()
-
+    
+    torch.manual_seed(seed)
     mp.spawn(
         fn=run_naive_ddp,
         args=(world_size, backend, cfg, True, warmup, nsteps, seed),
