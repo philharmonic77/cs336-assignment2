658d66c804bb3e4cf78f917573652bc3e25c4278
xintao <phil77@126.com>
2026-02-12 12:19:20 +0800

    naive ddp benchmark

---
+++ cs336_systems/ddp/naive_ddp_benchmark.py
@@ -0,0 +1,240 @@
+import torch 
+import os
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from cs336_systems.flash_attn.model_builder import build_model, ModelConfig
+from cs336_basics.losses import cross_entropy
+from cs336_basics.optim import AdamW
+from timeit import default_timer as timer
+# use mixed precision、torch.compile
+
+
+
+def run_naive_ddp(rank, world_size, backend, cfg, use_flash, warmup, nsteps, seed=123):
+    torch.manual_seed(seed) # for model param init
+    setup(rank, world_size, backend)
+
+    device = torch.device("cpu")
+    if backend == "nccl":
+        torch.cuda.set_device(rank)
+        device = torch.device(f"cuda:{rank}")
+
+    model = build_model(cfg, use_flash=use_flash).to(device)
+    init_model_and_broadcast(model, rank, src=0)
+    if device.type == "cuda":
+        model = torch.compile(model)
+
+    optimizer = AdamW(model.parameters(), lr=cfg.lr)
+
+    gen = torch.Generator()
+    gen.manual_seed(seed)
+
+    for _ in range(warmup):
+
+        x, y = generate_and_scatter_data(rank, world_size, cfg, device, gen)
+        _, _ = train_step(model, optimizer, x, y, device, world_size)       
+
+    total_time_acc = 0.0
+    comm_time_acc = 0.0
+    for _ in range(nsteps):
+
+        x, y = generate_and_scatter_data(rank, world_size, cfg, device, gen)
+        total_time, comm_time = train_step(model, optimizer, x, y, device, world_size)
+        total_time_acc += total_time
+        comm_time_acc += comm_time
+
+    total_tensor = torch.tensor(total_time_acc / nsteps, device=device)
+    comm_tensor = torch.tensor(comm_time_acc / nsteps, device=device)
+
+    dist.all_reduce(total_tensor, op=dist.ReduceOp.MAX)
+    dist.all_reduce(comm_tensor, op=dist.ReduceOp.MAX)
+
+    dist.barrier()
+    if rank == 0:
+        torch.save(model.state_dict(), "ddp.pt")
+
+        print(f"Avg step time: {total_tensor.item():.4f}s")
+        print(f"Avg comm time: {comm_tensor.item():.4f}s")
+
+    dist.barrier()
+    dist.destroy_process_group()
+
+    return model 
+
+
+def run_single(backend, cfg, use_flash, warmup, nsteps, seed=123):
+    torch.manual_seed(seed) # for model param init
+
+    device = torch.device("cpu")
+    if backend == "nccl":
+        torch.cuda.set_device(0)
+        device = torch.device(f"cuda:{0}")
+
+    model = build_model(cfg, use_flash=use_flash).to(device)
+    if device.type == "cuda":
+        model = torch.compile(model)
+
+    optimizer = AdamW(model.parameters(), lr=cfg.lr)
+
+    gen = torch.Generator()
+    gen.manual_seed(seed)
+
+    for _ in range(warmup):
+        x, y = generate_data_batch(cfg, device, gen)
+        _, _ = train_step(model, optimizer, x, y, device)
+
+    total_time_acc = 0.0
+    for _ in range(nsteps):
+        x, y = generate_data_batch(cfg, device, gen)
+        total_time, _ = train_step(model, optimizer, x, y, device)
+        total_time_acc += total_time
+
+    print(f"Avg step time: {total_time_acc / nsteps:.4f}s")
+
+    return model
+
+
+def generate_data_batch(cfg, device, generator):
+    x = torch.randint(
+        0, cfg.vocab_size,
+        (cfg.batch_size, cfg.context_len + 1),
+        device=device,
+        dtype=torch.long,
+        generator=generator
+    )
+    y = x[:, 1:]
+    x = x[:, :-1]
+    return x, y 
+
+def generate_and_scatter_data(rank, world_size, cfg, device, generator):
+    assert cfg.batch_size % world_size == 0
+
+    if rank == 0:
+        global_x, global_y = generate_data_batch(cfg, device, generator)
+
+        x_chunks = list(global_x.chunk(world_size, dim=0))
+        y_chunks = list(global_y.chunk(world_size, dim=0))
+    else:
+        x_chunks = None
+        y_chunks = None
+
+    x = torch.empty(cfg.batch_size // world_size, cfg.context_len, dtype=torch.long, device=device)
+    y = torch.empty(cfg.batch_size // world_size, dtype=torch.long, device=device)
+    dist.scatter(x, x_chunks, src=0)
+    dist.scatter(y, y_chunks, src=0)
+
+    return x, y
+
+def setup(rank: int, world_size: int, backend: str) -> None:
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "29500"
+    dist.init_process_group(backend, rank=rank, world_size=world_size)
+
+def init_model_and_broadcast(model, rank, src):
+    if rank == src:
+        pass
+    with torch.no_grad():
+        for p in model.parameters():
+            dist.broadcast(p.data, src=src)
+        for b in model.buffers():
+            dist.broadcast(b.data, src=src)
+            
+
+def train_step(model, optimizer, x, y, device, world_size=None):
+    start_total = timer()
+
+    with torch.autocast(
+        device_type=device.type,
+        dtype=torch.bfloat16 if device.type == "cuda" else torch.float32,
+        enabled=(device.type == "cuda"),
+    ):
+        logits = model(x)
+        loss = cross_entropy(logits, y)
+    
+    loss.backward()
+    torch.cuda.synchronize() if device.type == "cuda" else None
+
+    comm_time = 0.0
+
+    if world_size is not None:
+        start_comm = timer()
+
+        for p in model.parameters():
+            if p.grad is not None:
+                dist.all_reduce(p.grad)
+                p.grad /= world_size
+
+        torch.cuda.synchronize() if device.type == "cuda" else None
+        comm_time = timer() - start_comm
+
+    optimizer.step()
+    optimizer.zero_grad(set_to_none=True)
+
+    torch.cuda.synchronize() if device.type == "cuda" else None
+    total_time = timer() - start_total
+
+    return total_time, comm_time
+
+    
+def compare_models(m1, m2, atol=1e-4, rtol=1e-4):
+    m1 = m1.to("cpu").float()
+    m2 = m2.to("cpu").float()
+
+    for (n1, p1), (n2, p2) in zip(
+        m1.named_parameters(),
+        m2.named_parameters(),
+    ):
+        assert n1 == n2
+
+        if not torch.allclose(p1, p2, atol=atol, rtol=rtol):
+            max_diff = (p1 - p2).abs().max().item()
+            print(f"Mismatch in {n1}, max diff = {max_diff}")
+            return False
+
+    print("Models match!")
+    return True
+    
+
+def main():
+
+    world_size = 2
+    if torch.cuda.is_available():
+        backend = "nccl"
+    else:
+        backend = "gloo"
+    
+    cfg = ModelConfig()
+    warmup = 5
+    nsteps = 10
+    seed = 123
+
+    print("Single GPU + flash:")
+    model_use_flash = run_single(backend, cfg, use_flash=True, warmup=warmup, nsteps=nsteps, seed=seed)
+    print("Single GPU + no flash:")
+    model_no_flash = run_single(backend, cfg, use_flash=False, warmup=warmup, nsteps=nsteps, seed=seed)
+
+    # model compare：single + use_flash, single + no_flash
+    print("Comparing: single_flash vs single_no_flash")
+    compare_models(model_use_flash, model_no_flash)
+
+    print("Multi GPU + flash:")
+    mp.spawn(
+        fn=run_naive_ddp,
+        args=(world_size, backend, cfg, True, warmup, nsteps, seed),
+        nprocs=world_size,
+        join=True,
+    )
+
+    ddp_model = build_model(cfg, use_flash=True)
+    ddp_model.load_state_dict(torch.load("ddp.pt"))
+
+    # model compare：single + use_flash, ddp + use_flash
+    print("Comparing: single_flash vs ddp_flash")
+    compare_models(model_use_flash, ddp_model)
+
+    if os.path.exists("ddp.pt"):
+        os.remove("ddp.pt")
+
+
+if __name__ == "__main__":
+    main()
--- cs336_systems/flash_attn/model_builder.py
+++ cs336_systems/flash_attn/model_builder.py
@@ -4,17 +4,20 @@ import cs336_basics.nn.attention  as attn_mod
 from cs336_basics.nn.transformer import TransformerLM
 from cs336_systems.flash_attn.fa_triton import FlashAttentionTritonFunc
 
+# save original attention for future restore
+_original_attention = attn_mod.scaled_dot_product_attention
+
+# XL model downsize to large
 @dataclass(frozen=True)
 class ModelConfig:
-    d_model: int = 768
-    d_ff: int = 3072
-    num_layers: int = 12
-    num_heads: int = 12
-    context_len: int = 128
+    d_model: int = 1280
+    d_ff: int = 5120
+    num_layers: int = 36
+    num_heads: int = 20
+    context_len: int = 256
     vocab_size: int = 10000
     batch_size: int = 4
     lr: float = 3e-4
-    annotated:bool = False
 
 def flash_attention_wrapper(Q, K, V, mask=None):
     if mask is not None:
@@ -26,9 +29,13 @@ def flash_attention_wrapper(Q, K, V, mask=None):
         return FlashAttentionTritonFunc.apply(Q, K, V, True)
     return FlashAttentionTritonFunc.apply(Q, K, V, False)
 
-def build_model(cfg: ModelConfig) -> TransformerLM:
+def build_model(cfg: ModelConfig, use_flash=True) -> TransformerLM:
 
-    attn_mod.scaled_dot_product_attention = flash_attention_wrapper
+    if use_flash:
+        attn_mod.scaled_dot_product_attention = flash_attention_wrapper
+    else:
+        attn_mod.scaled_dot_product_attention = _original_attention
+
     model = TransformerLM(
         vocab_size=cfg.vocab_size,
         context_length=cfg.context_len,
