417fcd0ed956974d1415589e2c0b52ec92155ea2
xintao <phil77@126.com>
2026-02-13 12:12:09 +0800

    ddp overlap

---
+++ cs336_systems/ddp/overlap.py
@@ -0,0 +1,46 @@
+import torch 
+import torch.distributed as dist
+
+
+
+
+
+class DDP(torch.nn.Module):
+    def __init__(self, module: torch.nn.Module):
+        """
+        Given an instantiated PyTorch nn.Module to be parallelized, construct a DDP container that will handle gradient synchronization across ranks.
+        """
+        super().__init__()
+        self.module = module 
+        self.world_size = dist.get_world_size()
+        self.handles = []
+
+        for p in module.parameters():
+            if p.requires_grad:
+                p.register_post_accumulate_grad_hook(lambda p: self.hook(p))
+
+    def hook(self, p):
+        handle = dist.all_reduce(p.grad, async_op=True)
+        self.handles.append(handle)
+
+
+    def forward(self, *inputs, **kwargs): 
+        """
+        Calls the wrapped module’s forward() method with the provided positional and keyword arguments.
+        """
+        return self.module(*inputs, **kwargs)
+
+    def finish_gradient_synchronization(self): 
+        """
+        When called, wait for asynchronous communication calls to be queued on GPU.
+        """
+        for h in self.handles:
+            h.wait()
+
+        for p in self.module.parameters():
+            if p.grad is not None:
+                p.grad.div_(self.world_size)
+        self.handles.clear()
+
+        
+
--- tests/adapters.py
+++ tests/adapters.py
@@ -6,6 +6,7 @@ import torch
 
 from cs336_systems.flash_attn.fa_pytorch import FlashAttentionPytorchFunc
 from cs336_systems.flash_attn.fa_triton import FlashAttentionTritonFunc
+from cs336_systems.ddp.overlap import DDP
 
 
 
@@ -56,7 +57,7 @@ def get_ddp_individual_parameters(module: torch.nn.Module) -> torch.nn.Module:
         Instance of a DDP class.
     """
     # For example: return DDPIndividualParameters(module)
-    raise NotImplementedError
+    return DDP(module)
 
 
 def ddp_individual_parameters_on_after_backward(ddp_model: torch.nn.Module, optimizer: torch.optim.Optimizer):
@@ -71,7 +72,7 @@ def ddp_individual_parameters_on_after_backward(ddp_model: torch.nn.Module, opti
             Optimizer being used with the DDP-wrapped model.
     """
     # For example: ddp_model.finish_gradient_synchronization()
-    raise NotImplementedError
+    raise ddp_model().finish_gradient_synchronization()
 
 
 def get_ddp_bucketed(module: torch.nn.Module, bucket_size_mb: float) -> torch.nn.Module:
