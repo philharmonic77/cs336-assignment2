13cd0fdc71a921f7988c1d70a3acedcbeb090415
xintao <phil77@126.com>
2026-02-13 17:30:49 +0800

    update

--- cs336_systems/ddp/ddp_overlap_benchmark.py
+++ cs336_systems/ddp/ddp_overlap_benchmark.py
@@ -164,5 +164,5 @@ def main():
 if __name__ == "__main__":
     main()
 
-# bash: uv run nsys profile --trace=cuda,nvtx --pytorch=autograd-nvtx --force-overwrite=true -o results/nsys/ddp/overlap_ddp.nsys-rep python cs336_systems/ddp/ddp_overlap_benchmark.py
+# bash: uv run nsys profile --trace=cuda,nvtx --pytorch=autograd-nvtx --trace-fork-before-exec=true --wait=all --force-overwrite=true -o results/nsys/ddp/overlap_ddp.nsys-rep python cs336_systems/ddp/ddp_overlap_benchmark.py
 
